1142a136e15cbdfae030ff37c68769b87a3de9d5
Vitor <bigellivitor@gmail.com>
2020-03-20 19:49:37 -0300

    Remove call to spider

--- app.py
+++ app.py
@@ -67,9 +67,9 @@ def get_data():
 
  
 if __name__ == '__main__':
-    run_spider()
+    #run_spider()
     #scheduler = BackgroundScheduler()
-    #scheduler.add_job(func=run_spider, trigger="interval", seconds=5) 
+    #scheduler.add_job(func=run_spider, trigger="interval", seconds=3600) 
     #scheduler.start()
     
 
